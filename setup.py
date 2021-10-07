3593aae371c4a7becfc4b59fb6280e8139b9ebb6
Antonio Di Mariano <antonio.dimariano@gmail.com>
2021-10-07 22:52:39 +0200

    Added Dockerfile, LICENSE. Updated setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='websites_metrics_collector_beta',
-    version='1.0.3',
+    name='websites_metrics_collector',
+    version='0.0.1',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=["requests","confluent-kafka-producers-wrapper","aiohttp"],
-    url='',
-    license='',
+    url='https://github.com/antoniodimariano/websites_metrics_collector',
+    license='License :: OSI Approved :: Apache Software License',
     python_requires='>=3.6',
     author='Antonio Di Mariano',
     author_email='antonio.dimariano@gmail.com',
@@ -19,7 +19,7 @@ setup(
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ]
 )
